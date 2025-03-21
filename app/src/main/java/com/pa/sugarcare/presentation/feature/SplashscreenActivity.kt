1d0069fc852246b286095da335e1b2e628dee07e
jesicasp <115514246+jesicasp@users.noreply.github.com>
2025-03-21 20:48:15 +0700

    Initial commit

---
+++ app/src/main/java/com/pa/sugarcare/presentation/feature/SplashscreenActivity.kt
@@ -0,0 +1,35 @@
+package com.pa.sugarcare.presentation.feature
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.pa.sugarcare.MainActivity
+import com.pa.sugarcare.databinding.ActivitySplashscreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashscreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Pasang Splash Screen API
+        val splashScreen = installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Menunggu beberapa saat sebelum pindah ke MainActivity
+        splashScreen.setKeepOnScreenCondition { false }
+
+        binding.root.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}

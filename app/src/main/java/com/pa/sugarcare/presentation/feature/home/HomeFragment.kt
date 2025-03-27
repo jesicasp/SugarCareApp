59c077a01db25be95cbfcd5ddf3c247e3ef3fef5
jesicasp <115514246+jesicasp@users.noreply.github.com>
2025-03-27 20:44:37 +0700

    up home

--- app/src/main/java/com/pa/sugarcare/presentation/feature/home/HomeFragment.kt
+++ app/src/main/java/com/pa/sugarcare/presentation/feature/home/HomeFragment.kt
@@ -4,35 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.pa.sugarcare.databinding.FragmentHomeBinding
+import com.pa.sugarcare.presentation.adapter.OnBoardingViewPagerAdapter
+import com.pa.sugarcare.presentation.feature.home.screen.Information1
+import com.pa.sugarcare.presentation.feature.home.screen.Information2
+import com.pa.sugarcare.presentation.feature.home.screen.Information3
+
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var pagerAdapter: OnBoardingViewPagerAdapter
+    private val listOnBoardingScreen = listOf(
+        Information1(),
+        Information2(),
+        Information3()
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pagerAdapter = OnBoardingViewPagerAdapter(listOnBoardingScreen, this.childFragmentManager, lifecycle)
+
+        binding.apply {
+            viewPager.adapter = pagerAdapter
+            dotsIndicator.attachTo(viewPager)
+        }
     }
 
     override fun onDestroyView() {

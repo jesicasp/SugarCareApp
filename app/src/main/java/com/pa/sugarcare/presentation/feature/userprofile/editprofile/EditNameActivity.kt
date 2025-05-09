ce39b1a52e54b2670aa093fcc4c246fb7e1a0648
jesicasp <115514246+jesicasp@users.noreply.github.com>
2025-05-10 01:29:55 +0700

    up

--- app/src/main/java/com/pa/sugarcare/presentation/feature/userprofile/editprofile/EditNameActivity.kt
+++ app/src/main/java/com/pa/sugarcare/presentation/feature/userprofile/editprofile/EditNameActivity.kt
@@ -2,12 +2,23 @@ package com.pa.sugarcare.presentation.feature.userprofile.editprofile
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pa.sugarcare.R
+import com.pa.sugarcare.databinding.ActivityEditEmailBinding
+import com.pa.sugarcare.databinding.ActivityEditNameBinding
+import com.pa.sugarcare.presentation.feature.userprofile.vm.UserProfileViewModel
+import com.pa.sugarcare.repository.di.CommonVmInjector
 
 class EditNameActivity : AppCompatActivity() {
+    private var _binding: ActivityEditNameBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: UserProfileViewModel by viewModels {
+        CommonVmInjector.common(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
